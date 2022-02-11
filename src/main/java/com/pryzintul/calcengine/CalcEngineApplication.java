1dbd4e0ac9034ab16ad5670e55880f05e72b4308
Marcial <Lalanda>
2022-02-11 14:01:10 +0100

    initial commit

---
+++ src/main/java/com/pryzintul/calcengine/CalcEngineApplication.java
@@ -0,0 +1,13 @@
+package com.pryzintul.calcengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcEngineApplication.class, args);
+	}
+
+}
